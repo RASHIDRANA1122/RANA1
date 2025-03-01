64a5a6819be177915c98a5961dfa8a9d8aae5f38
RASHIDRANA1122 <rashidshar1000@gmail.com>
2025-03-01 11:38:52 +0500

    Update Rashid.sh

--- Rashid.sh
+++ Rashid.sh
@@ -1,15 +1,1 @@
-#-*-coding:utf-8-*-
-#!/usr/bin/python3
-#!/coding by rashid x Sefat
-import os,bs4,json,sys,time,random,re,subprocess,platform,struct,string,uuid,requests,httpx
-from bs4 import BeautifulSoup
-from bs4 import BeautifulSoup as sop
-from pip._vendor import requests as requests
-from concurrent.futures import ThreadPoolExecutor as M4H4D1
-from concurrent.futures import ThreadPoolExecutor as ThreadPool
-os.system('git pull');os.system('')
-try:
-    import requests
-except ImportError:
-        try:
-        
+import 
